9c6bf4d60281c5e3d554beee00e9791466b5cd87
Codingwithriha <rihashehzadi2003@gmail.com>
2026-08-30 12:00:13 +0500

    feat: enhance UI with new styles and layout for home, login, and signup pages

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ connectToMongoDB(process.env.MONGODB).then(() =>
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.static(path.resolve("./public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
